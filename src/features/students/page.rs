5a08d8db8c27ab36d9401f060173508cd8ff9e1a
Milton Ytusaca <milton.yv98@gmail.com>
2026-08-15 13:07:38 -0500

    style: animacion en el navbar + boton de crear alumno

--- src/features/students/page.rs
+++ src/features/students/page.rs
@@ -5,7 +5,9 @@ use leptos::prelude::*;
 #[component]
 pub fn StudentsPage() -> impl IntoView {
     view! {
-        // <h1 class="animate-in fade-in text-2xl">"Bienvenidos a alumnos"</h1>
+        <div class="absolute p-2 px-10 font-semibold shadow-lg shadow-black/20 top-[30px] border-1 border-cyan-600 -translate-x-1/2 left-1/2 bg-cyan-400/30 rounded-4xl z-10">
+            <button class="p-2 rounded-4xl">"Crear Alumno"</button>
+        </div>
 
         <StudentForm/>
         // <StudentList/>
--- src/shared/components/navbar.rs
+++ src/shared/components/navbar.rs
@@ -5,14 +5,17 @@ use leptos_router::{components::A, hooks::use_location};
 pub fn Navbar() -> impl IntoView {
     let location = use_location();
     view! {
-        <div class="absolute z-10 bottom-[30px] shadow-lg shadow-black/20 -translate-x-1/2  left-1/2 rounded-4xl
-                    bg-cyan-400/20 text-white text-md font-semibold border-1 border-cyan-600 p-2 backdrop-blur-md">
+        <div class="absolute z-10 bottom-[30px] shadow-lg shadow-black/20 -translate-x-1/2  left-1/2 rounded-4xl flex
+                    bg-cyan-400/20 w-[320px] text-white transition-all duration-300  text-md font-semibold border-1 
+                    border-cyan-600 p-2 backdrop-blur-md"
+            class=("w-[370px]", move || location.pathname.get() == "/students" )
+        >
             <ul class="flex gap-1 items-center justify-between">
                 <li
 
                     class="p-4 rounded-4xl transition-colors duration-700 ease-in-out"
                     class=(
-                        "bg-pink-400/60", move || location.pathname.get() == "/"
+                        ["bg-pink-400/60"], move || location.pathname.get() == "/"
                     )
                 >
                     <A href="/">"Inicio"</A>
@@ -21,10 +24,23 @@ pub fn Navbar() -> impl IntoView {
                     class="p-4 rounded-4xl transition-colors duration-700 ease-in-out"
 
                     class=(
-                        "bg-pink-400/60", move || location.pathname.get() == "/students"
+                        ["bg-pink-400/60", "shadow-black/30", "shadow-lg"], move || location.pathname.get() == "/students"
                     )
                 >
-                    <A href="/students">"Alumnos"</A>
+                    {move || {
+                        if location.pathname.get() == "/students" {
+                            view! {
+                                <button class="min-w-[120px] flex justify-center">"Crear alumno"</button>
+                            }.into_any()
+                        } else {
+                            view! {
+                                <div>
+                                    <A href="/students">"Alumnos"</A>
+                                </div>
+
+                            }.into_any()
+                        }
+                    }}
                 </li>
 
                 <li
