1456e465419ea8eaae5c23d2e923f64729f9ac50
Milton Ytusaca <milton.yv98@gmail.com>
2026-08-16 18:18:00 -0500

    feat: serivces activies añadido.

--- src/features/activities/services.rs
+++ src/features/activities/services.rs
@@ -5,6 +5,16 @@ use wasm_bindgen_futures::JsFuture;
 
 use super::models::*;
 
+#[derive(Serialize)]
+struct DeleteActivityArgs {
+    id: i64,
+}
+
+#[derive(Serialize)]
+struct CreateActivityArgs {
+    data: CreateActivity,
+}
+
 #[wasm_bindgen::prelude::wasm_bindgen]
 extern "C" {
     #[wasm_bindgen::prelude::wasm_bindgen(
@@ -14,9 +24,32 @@ extern "C" {
 }
 
 pub async fn get_activities() -> Result<Vec<Activity>, String> {
-    let result = JsFuture::from("get_activities", JsValue::NULL)
+    let result = JsFuture::from(invoke("get_activities", JsValue::NULL))
         .await
-        .map_err(|error| format!("{error}"))?;
+        .map_err(|error| format!("{error:?}"))?;
 
     from_value(result).map_err(|error| error.to_string())
+}
+
+pub async fn create_activity(data: CreateActivity) -> Result<Activity, String> {
+    let args = CreateActivityArgs { data };
+    let args = to_value(&args).map_err(|error| error.to_string())?;
+
+    let result = JsFuture::from(invoke("create_activity", args))
+        .await
+        .map_err(|error| format!("{error:?}"))?;
+
+    from_value(result).map_err(|error| error.to_string())
+}
+
+pub async fn delete_activity(id: i64) -> Result<(), String> {
+    let args = DeleteActivityArgs { id };
+
+    let args = serde_wasm_bindgen::to_value(&args).map_err(|error| error.to_string())?;
+
+    JsFuture::from(invoke("delete_activity", args))
+        .await
+        .map_err(|error| format!("{error:?}"))?;
+
+    Ok(())
 }
